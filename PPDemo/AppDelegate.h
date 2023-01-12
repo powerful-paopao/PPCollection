06b80aabeeb28865b5f2217ffa784a8e5d66e322
张义沛 <zyp@zhangyipeideMacBook-Pro.local>
2023-01-12 15:53:08 +0800

    创建项目

---
+++ PPDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PPDemo
+//
+//  Created by 张义沛 on 2023/1/12.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PPDemo/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  PPDemo
+//
+//  Created by 张义沛 on 2023/1/12.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
